36709cfd6732b619c9b710f778a1e24add2a3aed
Paweł Srebrny <silversk8@gmail.com>
2020-04-19 17:19:09 +0200

    02 replace config from css to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'The-good-architecture-of-the-Angular-app';
+    title = 'The-good-architecture-of-the-Angular-app';
 }
