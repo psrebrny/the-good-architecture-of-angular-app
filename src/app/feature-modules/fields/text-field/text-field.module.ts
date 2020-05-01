a8e21d332d3f16fe3ccaabde86627b2005092f9b
Paweł Srebrny <silversk8@gmail.com>
2020-05-01 19:58:03 +0200

    04 Management of shared components/directives/pipes and why we  should  divide them into separate modules, and simple shared data with Subjects

--- src/app/feature-modules/fields/text-field/text-field.module.ts
+++ src/app/feature-modules/fields/text-field/text-field.module.ts
@@ -1,6 +1,6 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {TextFieldComponent} from './text-field/text-field.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TextFieldComponent } from './text-field/text-field.component';
 
 @NgModule({
     declarations: [TextFieldComponent],
--- src/app/services/shared-data.service.spec.ts
+++ src/app/services/shared-data.service.spec.ts
@@ -1,6 +1,6 @@
-import {TestBed} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import {SharedDataService} from './shared-data.service';
+import { SharedDataService } from './shared-data.service';
 
 describe('SharedDataService', () => {
     let service: SharedDataService;
